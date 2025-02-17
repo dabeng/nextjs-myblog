7d5f7b2ee488e7e9dd74395858861517546cbe51
dabeng <dabeng413@gmail.com>
2025-02-17 17:26:43 +0800

    fig-bug: logout encounter 500 error

--- app/api/account/logout/route.ts
+++ app/api/account/logout/route.ts
@@ -1,10 +1,12 @@
 import { cookies } from 'next/headers';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import {z} from "zod";
-import { errorHandler,
-	jwtMiddleware,
-	validateMiddleware } from "_helpers/server";
+import { z } from "zod";
+import {
+  errorHandler,
+  jwtMiddleware,
+  validateMiddleware
+} from "_helpers/server";
 /* --- Logout Route Handler ---
 * It  simply deletes the 'authorization' cookie to log the user out. The cookie is HTTP only which
 * means it is only accessible on the server, this improves security by preventing cross site 
@@ -14,17 +16,12 @@ import { errorHandler,
 export async function POST(req: NextRequest) {
   try {
     await jwtMiddleware(req);
-		const schema = z.object({
-			username: z.string(),
-			password: z.string()
-		});
-		await validateMiddleware(req, schema);
-		
+
     (await cookies()).delete('authorization');
     return NextResponse.json({});
   } catch (err: any) {
-		// global error handler
-		return errorHandler(err);
-	}
-    
+    // global error handler
+    return errorHandler(err);
+  }
+
 }
