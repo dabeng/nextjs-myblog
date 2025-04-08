bc8259be331514105714176347322b5ddfdc8249
dabeng <dabeng413@gmail.com>
2025-04-08 18:35:47 +0800

    refactor profile page

--- app/(protected)/profile/[id]/page.tsx
+++ app/(protected)/profile/[id]/page.tsx
@@ -1,12 +1,4 @@
-
-// "use client"
-
-import Link from "next/link";
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-
-import { useBlogService, IBlog } from "_services";
-import { Spinner } from "_components";
-// import { useSession } from "next-auth/react";
+import { auth } from "@/auth";
 import BlogSection from "./blogSection";
 
 export default Profile;
@@ -16,16 +8,14 @@ async function Profile({
 }: {
   params: Promise<{ id: string }>
 }) {
-  // const { data: session } = useSession();
+  const session = await auth();
   const authorId = (await params).id;
-
-
 
   return (
     <>
       <section className="hero is-dark mt-5">
         <div className="hero-body">
-          {/* <p className="title">{session?.user?.username}</p> */}
+          <p className="title">{session?.user?.username}</p>
         </div>
       </section>
       <BlogSection authorId={authorId} />
